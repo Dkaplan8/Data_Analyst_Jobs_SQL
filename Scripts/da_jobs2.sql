c9e090f6c8b57adcb10af8b6faee73b358bff037
douglaskaplan <douglasjkaplan@gmail.com>
2024-06-06 18:40:33 -0500

    Finished all questions

--- Scripts/da_jobs2.sql
+++ Scripts/da_jobs2.sql
@@ -47,10 +47,11 @@
 -- 230
 
 -- 9. Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
--- SELECT COUNT(company)
+-- SELECT company, AVG(star_rating) AS avg_rating
 -- FROM data_analyst_jobs
 -- WHERE (review_count)>5000
--- 184
+-- GROUP BY company
+-- 41
 
 
 -- 10. Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
